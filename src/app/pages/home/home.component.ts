80e42d261f7471b8674e52105c46b292722fca68
Angelica Huertas <angiema.huertas@gmail.com>
2026-02-27 10:45:21 -0300

    carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { FeaturedArticleComponent } from '../../components/featured-article/featured-article.component';
 import { ArticleComponent } from '../../components/article/article.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home',
   imports: [
     FeaturedArticleComponent,
-    ArticleComponent
+    ArticleComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styles: ``
 })
